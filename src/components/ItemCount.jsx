f124d3cd8cad4090f4108ca0f98a752623ee0bcf
Cesar <camongez28@gmail.com>
2022-11-01 22:36:14 -0300

    ItemCount Add

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,37 @@
+import { Button } from '@mui/material';
+import { useState } from 'react';
+
+export const ItemCount = ({ stock, onAdd }) => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  };
+
+  const handleDecrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <Button onClick={() => handleDecrease()}>-</Button>
+        {count}
+        <Button onClick={() => handleIncrease()}>+</Button>
+      </div>
+      <Button
+        variant='outlined'
+        onClick={() => {
+          if (count) {
+            onAdd();
+          }
+        }}>
+        Agregar al carrito
+      </Button>
+    </>
+  );
+};
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,3 +1,10 @@
+import { Button } from '@mui/material';
+import { ItemCount } from './ItemCount';
 export const ItemListContainer = ({ greeting }) => {
-  return <h2 style={{ color: 'red' }}>{greeting}</h2>;
+  return (
+    <>
+      <h2 style={{ color: 'red' }}>{greeting}</h2>
+      <ItemCount stock={6} onAdd={() => console.log('onAdd')} />
+    </>
+  );
 };
