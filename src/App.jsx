710bf097bb8ac383cc69dfaf16c232b2d8d8252c
Cesar <camongez28@gmail.com>
2022-12-28 13:10:57 -0300

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='App'>
       <CartProvider>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/checkout/:id' element={<Checkout />} />
